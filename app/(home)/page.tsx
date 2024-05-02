aa72c0c43c7ee48a227fefe22b30eb925525b952
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-05-01 22:54:22 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -28,6 +28,7 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
+
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
         
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden"> 
@@ -35,7 +36,22 @@ export default async function Home() {
               <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
           ))}
         </div>
+
       </div>
+
+      <div className="mt-6 mb-[3.75rem]">
+        
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden"> 
+          {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+
+      </div>
+
+      
     </div>
   );
 }
