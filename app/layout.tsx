85593a687d1af37124e2326ce412e59f44318af4
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-05-03 13:15:33 -0300

    feat: add auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
-        {children}
-        <Footer/>
-        </body>
+        <AuthProvider>
+          {children}
+          <Footer />
+        </AuthProvider>
+
+      </body>
     </html>
   );
 }
