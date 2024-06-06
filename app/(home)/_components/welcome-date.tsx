0cc575e2f40ac12d302a16a37dd404cfa16de2fb
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-06-06 18:11:15 -0300

    feat: add msg welcome text

--- app/(home)/_components/welcome-date.tsx
+++ app/(home)/_components/welcome-date.tsx
@@ -2,14 +2,13 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
 
 const WelcomeDate = async () => {
     const  data  = await getServerSession(authOptions);
     const username = data?.user?.name?.split(' ').slice(0,2).join(' ');
 
     return (<div className="px-5 pt-5">
-        {username && <h2 className="text-xl font-bold">Olá, {username}</h2>}
+        <h2 className="text-xl font-bold">Olá, {username ? username : ' vamos agendar um corte hoje?'}</h2>
         <p className="capitalize text-sm ">{format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
         })}
